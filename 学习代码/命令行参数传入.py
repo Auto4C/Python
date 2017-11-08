eb264c70aac7fce67b6274e48eb913405b012b60
chj <chj1417@126.com>
2017-11-08 11:53:24 +0800

    ss

--- 学习代码/命令行参数传入.py
+++ 学习代码/命令行参数传入.py
@@ -1,13 +1,11 @@
-#命令行参数输入显示
+# 命令行参数输入显示
 import sys
 import time
-#增加窗口
+# 增加窗口
 from PyQt5 import QtCore, QtWidgets
-#窗口载入
+# 窗口载入
 app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QWidget()
 widget.resize(400, 100)
 widget.setWindowTitle("这是个普通的窗口测试")
 widget.show()
-
-
